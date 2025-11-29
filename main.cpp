74b99e6493d938ac007d5508f39dfc46143ad275
ahmad777x86 <087m.ahmad@gmail.com>
2025-11-30 00:10:30 +0500

    Made colision system and initialised menu

--- main.cpp
+++ main.cpp
@@ -1,69 +1,98 @@
 #include "raylib.h"
+#include <math.h>
+#include <iostream>
 
 using namespace std;
 
-int fruits[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int fruit_posx[20];
-int fruit_posy[20];
+#define MENU 0
+#define GAME 1
 
-void Create_Fruit(int posx, int posy, int i)
+int fruits[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+float fruit_posx[20];
+float fruit_posy[20];
+Rectangle fruit_hitbox[20];
+
+Rectangle player;
+float pX = 400;
+float pY = 350;
+
+static int score = 0;
+
+void Create_Fruit(float posx, float posy, int i, float width = 10, float height = 10)
 {
     fruits[i] = 1;
     fruit_posx[i] = posx;
     fruit_posy[i] = posy;
+    fruit_hitbox[i] = (Rectangle){posx, posy, width, height};
+}
+
+void Create_Player(float posx, float posy, float width = 60, float height = 30)
+{
+    pX = posx;
+    pY = posy;
+    player = (Rectangle){pX, pY, width, height};
+}
+
+void Draw_Menu()
+{
+}
+
+void Draw_Game()
+{
+    int i = 0;
+    if (IsKeyDown(KEY_D))
+    {
+        pX += 3;
+    }
+    if (IsKeyDown(KEY_A))
+    {
+        pX -= 3;
+    }
+
+    player = (Rectangle){pX, pY, player.width, player.height};
+
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && i < 20)
+    {
+        Vector2 mousepos = GetMousePosition();
+        Create_Fruit(mousepos.x, mousepos.y, i);
+        i++;
+    }
+    ClearBackground(RAYWHITE);
+    DrawText(TextFormat("Score: %d", score), 30, 15, 20, RED);
+    DrawRectangle(player.x, player.y, player.width, player.height, RED);
+
+    for (int i = 0; i < 20; i++)
+    {
+        if (fruits[i])
+        {
+            DrawRectangle(fruit_posx[i], fruit_posy[i], 15, 15, GREEN);
+            fruit_posy[i] += 2;
+            fruit_hitbox[i].y = fruit_posy[i];
+            if (CheckCollisionRecs(fruit_hitbox[i], player))
+            {
+                fruits[i] = 0;
+                fruit_posx[i] = 0;
+                fruit_posy[i] = 0;
+                score++;
+            }
+        }
+    }
 }
 
 int main()
 {
     const int width = 800;
     const int height = 450;
-    int posX = 400;
-    int posY = 350;
-    static int score = 0;
-    int i = 0;
 
     InitWindow(width, height, "Basket Blitz");
 
     SetTargetFPS(60);
+    Create_Player(pX, pY, 60, 30);
 
     while (!WindowShouldClose())
     {
-        if (IsKeyDown(KEY_D))
-        {
-            posX += 3;
-        }
-        if (IsKeyDown(KEY_A))
-        {
-            posX -= 3;
-        }
-
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            Vector2 mousepos = GetMousePosition();
-            Create_Fruit(mousepos.x, mousepos.y, i);
-            i++;
-        }
 
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText(TextFormat("%d", score), 400, 50, 20, RED);
-        DrawRectangle(posX, posY, 60, 30, RED);
-
-        for (int i = 0; i < 20; i++)
-        {
-            if (fruits[i])
-            {
-                DrawCircle(fruit_posx[i], fruit_posy[i], 8, GREEN);
-                fruit_posy[i]++;
-                if (fruit_posy[i] - posY && fruit_posx[i] == posY)
-                {
-                    fruits[i] = 0;
-                    fruit_posx[i] = 0;
-                    fruit_posy[i] = 0;
-                    score++;
-                }
-            }
-        }
 
         EndDrawing();
     }
